a5711c8c2cb8f64a28c0fa998ce55b7b4846409c
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-04-30 20:04:49 -0300

    fix: refactoring tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,20 +1,22 @@
 import { prisma } from "../src/database.js";
 import supertest from "supertest";
 import app from "../src/app.js";
-import createUser from "./factories/userFactory.js";
+import userFactory from "./factories/userFactory.js";
 
 describe("POST /sign-up", () => {
     beforeEach(async () => {
-        await prisma.$executeRaw`TRUNCATE TABLE users`;
+        await truncateTable("users");
     });
-    afterAll(async () => {
-        await prisma.$disconnect();
-    });
+    afterAll(disconnect);
 
-    it("should return status code 201 given a valid user", async () => {
-        const user = createUser();
-        const response = await supertest(app).post("/sign-up").send(user);
+    it("should return status code 201 given a valid user and persist data", async () => {
+        const body = userFactory.createUserBody();
+        const response = await supertest(app).post("/sign-up").send(body);
+        const user = await prisma.user.findUnique({
+            where: { email: body.email },
+        });
         expect(response.statusCode).toEqual(201);
+        expect(user).not.toBe(null);
     });
 
     it("should return status code 422 given an invalid user input", async () => {
@@ -24,31 +26,35 @@ describe("POST /sign-up", () => {
         expect(response.statusCode).toEqual(422);
     });
 
-    it("should return status code 409 given registered user", async () => {
-        const user = createUser();
-        await supertest(app).post("/sign-up").send(user);
-        const response = await supertest(app).post("/sign-up").send(user);
+    it("should return status code 409 given registered user and not persist", async () => {
+        const body = userFactory.createUserBody();
+
+        await supertest(app).post("/sign-up").send(body);
+        const response = await supertest(app).post("/sign-up").send(body);
+        const users = await prisma.user.findMany({
+            where: { email: body.email },
+        });
+
         expect(response.statusCode).toEqual(409);
+        expect(users.length).toEqual(1);
     });
 });
 
 describe("POST /sign-in", () => {
     beforeEach(async () => {
-        await prisma.$executeRaw`TRUNCATE TABLE users`;
+        await truncateTable("users");
     });
-    afterAll(async () => {
-        await prisma.$disconnect();
-    });
+    afterAll(disconnect);
 
     it("should return status code 200 given a valid user", async () => {
-        const user = createUser();
-        await supertest(app).post("/sign-up").send(user);
-        const response = await supertest(app).post("/sign-in").send(user);
+        const body = userFactory.createUserBody();
+        await userFactory.createUser(body);
+        const response = await supertest(app).post("/sign-in").send(body);
         expect(response.statusCode).toEqual(200);
     });
 
     it("should return status code 401 given a non registered user", async () => {
-        const user = createUser();
+        const user = userFactory.createUserBody();
         const response = await supertest(app).post("/sign-in").send(user);
         expect(response.statusCode).toEqual(401);
     });
@@ -60,3 +66,10 @@ describe("POST /sign-in", () => {
         expect(response.statusCode).toEqual(422);
     });
 });
+
+async function truncateTable(tableName: string) {
+    await prisma.$queryRawUnsafe(`TRUNCATE TABLE ${tableName};`);
+}
+async function disconnect() {
+    await prisma.$disconnect();
+}
--- tests/factories/userFactory.ts
+++ tests/factories/userFactory.ts
@@ -1,10 +1,29 @@
 import { faker } from "@faker-js/faker";
+import { prisma } from "../../src/database.js";
+import bcrypt from "bcrypt";
 
-function createUser() {
+export interface UserBodyData {
+    email: string;
+    password: string;
+}
+
+function createUserBody() {
     return {
         email: faker.internet.email(),
         password: faker.internet.password(),
     };
 }
 
-export default createUser;
+async function createUser(body: UserBodyData) {
+    await prisma.user.create({
+        data: {
+            ...body,
+            password: bcrypt.hashSync(body.password, 10),
+        },
+    });
+}
+
+export default {
+    createUser,
+    createUserBody,
+};
